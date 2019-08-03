42d42bdf701d9b2deca6b2903105b1935abdc423
Viktor Richard Kozma <>
2019-08-04 01:55:23 +0200

    made some stuff

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//REDUX
+import {Provider} from 'react-redux';
+import store from './store'; 
+import { connect } from 'react-redux';
+
+
+//LOCALIZATION
+import {addLocaleData} from 'react-intl'
+import en from 'react-intl/locale-data/en'
+import hu from 'react-intl/locale-data/hu'
+import {localeSet} from './actions/actions'
+import messages from './messages'
+import {IntlProvider} from 'react-intl';
+
+
+//STYLESHEET
+import './App.scss';
+
+//LOCALIZATION INIT
+addLocaleData(en)
+addLocaleData(hu)
+
+
+
+store.dispatch(localeSet('hu'))
+
+
+
+class App extends Component {
+
+  render(){
+    const {lang} = this.props;
+
+    return (
+      <Provider store={store}>
+        <IntlProvider locale={lang} messages={messages[lang]}>
+          <div className="App">
+            cica
+
+          </div>
+        </IntlProvider>
+      </Provider>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  lang: state.locale.lang
+});
+
+export default connect(mapStateToProps,{})(App);
---
+++ src/messages.js
@@ -0,0 +1,52 @@
+export default {
+    en:{
+        'menu_projects': 'Projects',
+        'menu_office': 'Office',
+        'menu_contact': 'Contact',
+        
+        'selected_view': 'Selected',
+        'list_view': 'List',
+        '0_all': 'All',
+        '1_public': 'Public',
+        '2_commercial': 'Commercial',
+        '3_hotel': 'Hotel and Residential',
+        '4_industrial': 'Industrial',
+        '5_heritage': 'Heritage',
+        '6_others': 'Others',
+
+        'project_list': 'Project',
+        'location_list': 'Location',
+        'country_list': 'Country',
+        'year_list': 'Year',
+        'type_list': 'Type',
+        
+        'about_office': 'About Us',
+        'people_office': 'People',
+        'prizes_office': 'Prizes',
+ 
+    },
+    hu:{
+        'menu_projects': 'Projektek',
+	    'menu_office': 'Studió',
+        'menu_contact': 'Kapcsolat',
+        'selected_view': 'Válogatott munkáink',
+        'list_view': 'Lista',
+        '0_all': 'Összes',
+        '1_public': 'Középület',
+        '2_commercial': 'Kereskedelmi',
+        '3_hotel': 'Szállás és lakóépület',
+        '4_industrial': 'Ipari épület',
+        '5_heritage': 'Műemlék',
+        '6_others': 'Egyéb',
+
+        'project_list': 'Projekt',
+        'location_list': 'Város',
+        'country_list': 'Ország',
+        'year_list': 'Év',
+        'type_list': 'Kategória',
+    
+        'about_office': 'Cégünkről',
+        'people_office': 'Munkatársak',
+        'prizes_office': 'Díjak'
+    }
+}
