01de7e4aee6e7e194c2d8a85e1bc909c98ed5928
Viktor Richard Kozma <>
2019-08-19 09:33:59 +0200

    fixed backgouonr

--- src/components/projects/project-selected/square-view/index.js
+++ src/components/projects/project-selected/square-view/index.js
@@ -13,7 +13,7 @@ const SquareView = (props) => {
           <div className="square-inner" style={{
               backgroundImage :`url(${props.data.thumbnail})`,
               backgroundPosition: 'center',
-              backgroundSize: '300px'}} >
+              backgroundSize: '450px'}} >
             
             <div className="square-inner-content" >
               <div className="square-inner-content-wrapper" >
